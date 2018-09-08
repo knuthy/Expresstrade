b0009a1ee069abf34555960dd4915f4734359ed7
knuthy <knuth.frederik@gmail.com>
2018-09-09 01:34:58 +0200

    Create inventories_cache.js

---
+++ inventories_cache.js
@@ -0,0 +1,68 @@
+var cachedInventories = [];
+
+socket.on('loadUserInventory', function(forceRefresh) {
+	var cache;
+	var inventoryCache;
+	
+	// Is the user's inventory already cached?
+	cachedInventories.forEach(function(inv) {
+		if (inv.steamId == socketuser.id64) {
+			cache = true;
+			inventoryCache = inv;
+		}
+	});
+	
+	// If the user doesn't want a force refresh, we're sending the cached inventory.
+	if (!forceRefresh && cache) {
+		socket.emit('userInventory', {
+			content: inventoryCache
+		});
+	} else {
+		// Removing the old cached inventory
+		clearInventory(socketuser.id64);
+		ET.ITrade.GetUserInventoryFromSteamId({steam_id: socketuser.id64}, (err, body) => {
+			if (err) {
+				return;
+			} else {
+				if (body.status == 1) {
+					// Inventory loaded successfully
+					var inventory = [];
+					body.response.items.forEach(function(item) {
+						inventory.push({
+							id: item.id,
+							category: item.category,
+							name: item.name,
+							img: item.image['600px'],
+							color: item.color,
+							price: item.suggested_price
+						});
+					});
+					socket.emit('userInventory', {
+						content: inventory
+					});
+					// Caching the newly loaded inventory
+					cachedInventories.push({
+						steamId: socketuser.id64,
+						inv: inventory,
+						time: Date.now(),
+						cache: true
+					});
+				} else {
+					// Inventory could not load
+					socket.emit('error', {
+						content: 'INVENTORY_COULD_NOT_LOAD'
+					});
+				}
+			}
+		});
+	}
+});
+// Function for removing old cache
+function clearInventory(steamId) {
+	for (let i = cachedInventories.length - 1; i >= 0; i--) {
+		if (cachedInventories[i]['steamId'] == steamId) {
+			cachedInventories.splice(i, 1);
+		}
+	}
+	return cachedInventories;
+}
